5f388e50e6820f2c7187bc67895cfb2dea46413e
Sergey Shandar <sergey.shandar@gmail.com>
2018-08-31 14:09:45 -0700

    package update

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 export { ReadMeManipulator } from "./readMeManipulator"
+export { SuppressionItem } from "./readMeManipulator"
