a093ba721443a437d61e0d8b30e9863c60c539bb
Sergey Shandar <sergey.shandar@gmail.com>
2018-08-31 14:28:46 -0700

    ReadMeBuilder

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-export { ReadMeManipulator } from "./readMeManipulator"
-export { SuppressionItem } from "./readMeManipulator"
+export { ReadMeManipulator, SuppressionItem } from "./readMeManipulator"
+export { ReadMeBuilder } from "./readMeBuilder"
