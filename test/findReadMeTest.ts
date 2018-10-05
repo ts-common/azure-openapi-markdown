340bbf7c67724cac0b256d794d12aaf6cf2bb530
Sergey Shandar <sergey.shandar@gmail.com>
2018-10-05 11:11:09 -0700

    copyrights.

--- test/findReadMeTest.ts
+++ test/findReadMeTest.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 import { findReadMe } from "../findReadMe"
 import * as assert from "assert"
 
